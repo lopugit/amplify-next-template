374e8520718d029fbd5b77f6abf059e765fba6d3
Nikolaj Frey <lopudesigns@gmail.com>
2024-10-28 15:29:18 +1100

    nested

--- next/postBuildStats.sh
+++ next/postBuildStats.sh
@@ -1,36 +1,37 @@
+# dont exit 2 on any errors
 echo "Running postBuildStats.sh"
 
 echo "ls -la"
-ls -la
+ls -la || true
 
 echo "ls -la .next"
-ls -la .next
+ls -la .next || true
 
 echo "ls -la amplify"
-ls -la amplify
+ls -la amplify || true
 
 # check that deploy-manifest.json exists
 echo "ls -la amplify/backend/hosting/S3AndCloudFront/deploy-manifest.json"
-ls -la amplify/backend/hosting/S3And
+ls -la amplify/backend/hosting/S3And || true
 
 echo "ls -la amplify/backend/hosting/S3AndCloudFront"
-ls -la amplify/backend/hosting/S3AndCloudFront
+ls -la amplify/backend/hosting/S3AndCloudFront || true
 
 echo "ls -la deploy-manifest.json"
-ls -la deploy-manifest.json
+ls -la deploy-manifest.json || true
 
 echo "pwd"
 pwd
 
 # check certain directories exist
 echo "Checking for .amplify-hosting directory"
-ls -la .amplify-hosting
+ls -la .amplify-hosting || true
 
 echo "Checking for .amplify directory"
-ls -la .amplify
+ls -la .amplify || true
 
 echo "Checking for .github directory"
-ls -la .github
+ls -la .github || true
 
 echo "Checking for .git directory"
-ls -la .git
+ls -la .git || true

62122ebc1dd9706b8a452a4b34a1980a3bf6e197
Nikolaj Frey <lopudesigns@gmail.com>
2024-10-29 14:04:30 +1100

    Removed all amplify imports...  main

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
+// import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 // import { Amplify } from "aws-amplify";
 // import outputs from "@/amplify_outputs.json";
@@ -10,15 +10,15 @@ import "@aws-amplify/ui-react/styles.css";
 
 // Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+    // client.models.Todo.observeQuery().subscribe({
+    //   next: (data) => setTodos([...data.items]),
+    // });
   }
 
   useEffect(() => {
@@ -26,14 +26,14 @@ export default function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
+    // client.models.Todo.create({
+    //   content: window.prompt("Todo content"),
+    // });
   }
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>My todos - With no backend</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (

297f046abe25dd1a8b6ec16012db0be8396d9a0a
Nikolaj Frey <lopudesigns@gmail.com>
2024-10-29 13:56:24 +1100

    Testing without backend build step main

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+// import { Amplify } from "aws-amplify";
+// import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
